f9c9f9be51f714362c444af57e61bbf3fecf54e1
Захаров Андрей <zaharov.andrey161@gmail.com>
2023-04-27 08:34:41 +0300

    Initial commit.
    Made simple img generator.

---
+++ image_generator.py
@@ -0,0 +1,18 @@
+from PIL import Image
+from random import randint
+
+# TODO: symmetry_mode
+def generate_pixels(size):
+    base_color = (randint(0, 256), randint(0, 256), randint(0, 256))
+    accent_color = (255 - base_color[0], 255 - base_color[1], 255 - base_color[2])
+    img = Image.new('RGB', (size, size))
+    for i in range(size):
+        for j in range(size):
+            img.putpixel((i, j), base_color if randint(0, 2) == 0 else accent_color)
+    # uncomment for auto-open
+    # img.show()
+    return img
+
+
+if __name__ == '__main__':
+    generate_pixels(int(input()))
